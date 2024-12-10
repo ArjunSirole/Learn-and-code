9899351c0efbca001ca6b1296944ac2518e2d3e3
Arjun A Sirole <arjun.a.sirole@intimetec.com>
2024-12-10 11:31:55 +0530

    adjacent countries

--- meaningful_names.py
+++ meaningful_names.py
@@ -1,20 +1,20 @@
 # Assignment 1: The below program is to Roll the Dice
 import random
 
-def roll_dice(sides):
-    rolled_number = random.randint(1, sides)
-    return rolled_number
+def rollDice(sides):
+    rolledNumber = random.randint(1, sides)
+    return rolledNumber
 
 def main():
-    dice_sides = 6
-    keep_playing = True
-    while keep_playing:
-        user_input = input("Ready to roll? Enter Q to Quit: ")
-        if user_input.lower() != "q":
-            rolled_number = roll_dice(dice_sides)
-            print(f"You have rolled a {rolled_number}")
+    diceSides = 6
+    keepPlaying = True
+    while keepPlaying:
+        userInput = input("Ready to roll? Enter Q to Quit: ")
+        if userInput.lower() != "q":
+            rolledNumber = rollDice(diceSides)
+            print(f"You have rolled a {rolledNumber}")
         else:
-            keep_playing = False
+            keepPlaying = False
 
 main()
 
@@ -23,60 +23,59 @@ main()
 # Assignment 2: The below program is to guess the correct number between 1 to 100
 import random
 
-def is_valid_guess(guess):
+def isValidGuess(guess):
     if guess.isdigit() and 1 <= int(guess) <= 100:
         return True
     return False
 
 def main():
-    target_number = random.randint(1, 100)
-    guess_is_correct = False
-    user_guess = input("Guess a number between 1 and 100: ")
-    guess_count = 0
+    targetNumber = random.randint(1, 100)
+    guessIsCorrect = False
+    userGuess = input("Guess a number between 1 and 100: ")
+    guessCount = 0
 
-    while not guess_is_correct:
-        if not is_valid_guess(user_guess):
-            user_guess = input("Invalid input! Please enter a number between 1 and 100: ")
+    while not guessIsCorrect:
+        if not isValidGuess(userGuess):
+            userGuess = input("Invalid input! Please enter a number between 1 and 100: ")
             continue
         else:
-            guess_count += 1
-            user_guess = int(user_guess)
+            guessCount += 1
+            userGuess = int(userGuess)
 
-        if user_guess < target_number:
-            user_guess = input("Too low. Guess again: ")
-        elif user_guess > target_number:
-            user_guess = input("Too high. Guess again: ")
+        if userGuess < targetNumber:
+            userGuess = input("Too low. Guess again: ")
+        elif userGuess > targetNumber:
+            userGuess = input("Too high. Guess again: ")
         else:
-            print(f"You guessed it in {guess_count} guesses!")
-            guess_is_correct = True
+            print(f"You guessed it in {guessCount} guesses!")
+            guessIsCorrect = True
 
 main()
 
 
 # Assignment 3: The below program is to check whether the number is Armstrong number or not
-def calculate_sum_of_powers(number):
-    sum_of_powers = 0
-    number_digits = 0
+def calculateSum(number):
+    sum = 0
+    numberDigits = 0
 
-    temporary_number = number
-    while temporary_number > 0:
-        number_digits += 1
-        temporary_number //= 10
+    temporaryNumber = number
+    while temporaryNumber > 0:
+        numberDigits += 1
+        temporaryNumber //= 10
 
-    temporary_number = number
-    for _ in range(number_digits):
-        digit = temporary_number % 10
-        sum_of_powers += digit ** number_digits
-        temporary_number //= 10
+    temporaryNumber = number
+    for _ in range(1, temporaryNumber + 1):
+        digit = temporaryNumber % 10
+        sum += digit ** numberDigits
+        temporaryNumber //= 10
     
-    return sum_of_powers
+    return sum
 
-def main():
-    number_to_check = int(input("\nPlease Enter the Number to Check for Armstrong: "))
+inputNumber = int(input("\nPlease Enter the Number to Check for Armstrong: "))
     
-    if number_to_check == calculate_sum_of_powers(number_to_check):
-        print(f"\n{number_to_check} is an Armstrong Number.\n")
-    else:
-        print(f"\n{number_to_check} is Not an Armstrong Number.\n")
+if inputNumber == calculateSum(inputNumber):
+    print(f"\n{inputNumber} is an Armstrong Number.\n")
+else:
+    print(f"\n{inputNumber} is Not an Armstrong Number.\n")
 
-main()
+
